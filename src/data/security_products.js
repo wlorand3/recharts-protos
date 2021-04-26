ea63c47b86f99a0485af03665b6896adec765f52
Will Lorand <wlorand@cisco.com>
2021-04-26 14:04:52 -0700

    feat: code tweaks and demo stopping pt

--- src/data/security_products.js
+++ src/data/security_products.js
@@ -1,32 +1,32 @@
 export const security_products = [
   {
     name: "",
-    installed: 900,
+    installed: 9000,
     fill: "rgb(255,255,255)",
   },
   {
     name: "AMP Cloud",
-    installed: 900,
+    installed: 9000,
     fill: "rgb(227,106,85)",
   },
   {
     name: "Jamf Pro",
-    installed: 700,
+    installed: 7000,
     fill: "rgb(252,132,230)",
   },
   {
     name: "InTune",
-    installed: 500,
+    installed: 5000,
     fill: "rgb(133,156,223)",
   },
   {
     name: "Duo",
-    installed: 300,
+    installed: 3000,
     fill: "rgb(2,156,39)",
   },
   {
     name: "Mobile Iron",
-    installed: 100,
+    installed: 1000,
     fill: "rgb(179,131,7)",
   },
 ];
