461aeafb8b4df603692cd8a7c1c68147bb2ab36b
Will Lorand <wlorand@cisco.com>
2021-12-03 13:43:25 -0800

    feature: first cut of recharts funnel chart

---
+++ src/data/sales_process_data.js
@@ -0,0 +1,22 @@
+export const sales_process_data = [
+  {
+    name: "Total Market",
+    value: 232000,
+    fill: "#8884d8",
+  },
+  {
+    name: "Prospects",
+    value: 94480,
+    fill: "#83a6ed",
+  },
+  {
+    name: "Leads",
+    value: 47390,
+    fill: "#8dd1e1",
+  },
+  {
+    name: "Sales",
+    value: 22181,
+    fill: "#82ca9d",
+  },
+];
