8064c7167356eb21dd593b94b060728776a62fe2
Will Lorand <wlorand@cisco.com>
2021-12-11 13:50:05 -0800

    feat: D3 funnel chart with lots of custom options - disabled til demo

--- src/components/D3FunnelChart.js
+++ src/components/D3FunnelChart.js
@@ -2,21 +2,58 @@ import { useEffect, useRef } from "react";
 
 import D3Funnel from "d3-funnel";
 
-// Data
-import { sales_process_data } from "../data/sales_process_data";
+// data
+// import { sales_process_data } from "../data/sales_process_data";
+import { threat_hunt_data } from "../data/threat_hunt_data";
 
 // styles
 import "../styles/charts.css";
 
 function D3FunnelChart() {
   const chartRef = useRef(null);
-  console.log("sales process data is: ", sales_process_data);
 
   useEffect(() => {
     if (chartRef.current) {
-      const chartOptions = {};
+      const chartOptions = {
+        chart: {
+          animate: 250,
+          bottomPinch: 0, // demo-2
+          curve: {
+            enabled: false, // demo-1
+            height: 20,
+          },
+          inverted: false, // demo-3
+          height: 400,
+          width: 600,
+        },
+        block: {
+          barOverlay: false, // demo-4
+          dynamicHeight: false, // demo-5
+          fill: {
+            type: "solid", // demo-6  "gradient"
+            // scale: ["#607ac8", "#188c7c", "#5f842e", "#937530"], // demo-0 (csx chart-colors)
+          },
+          highlight: false, // demo-7
+        },
+        events: {
+          click: {
+            block(event, d) {
+              window.open("https://security.cisco.com/", "_blank");
+            },
+          },
+        },
+        label: {
+          enabled: true, // demo-8
+          fill: "#fff", // use #000 for dark theme ?!
+          fontSize: "12px",
+        },
+        tooltip: {
+          enabled: true,
+          format: "{f} are {l}", // takes func or f-str
+        },
+      };
       const chart = new D3Funnel("#funnel");
-      chart.draw(sales_process_data, chartOptions);
+      chart.draw(threat_hunt_data, chartOptions);
     }
   }, []);
 
--- src/data/sales_process_data.js
+++ src/data/sales_process_data.js
@@ -2,21 +2,25 @@ export const sales_process_data = [
   {
     label: "Total Market",
     value: 66200,
-    fill: "#8884d8",
+    fill: "#607ac8",
+    backgroundColor: "#607ac8",
   },
   {
     label: "Prospects",
     value: 24480,
-    fill: "#83a6ed",
+    fill: "#188c7c",
+    backgroundColor: "#188c7c",
   },
   {
     label: "Leads",
     value: 7390,
-    fill: "#8dd1e1",
+    fill: "#5f842e",
+    backgroundColor: "#5f842e",
   },
   {
     label: "Sales",
     value: 2181,
-    fill: "#82ca9d",
+    fill: "#937530",
+    backgroundColor: "#937530",
   },
 ];
