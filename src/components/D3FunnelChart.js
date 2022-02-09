3d227708f01519aa7ae97203c92504b3efe76627
Will Lorand <wlorand@cisco.com>
2022-02-09 14:05:35 -0800

    feat: put chartOptions into a useMemo and out of the useEffect

--- src/components/D3FunnelChart.js
+++ src/components/D3FunnelChart.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 
 import D3Funnel from "d3-funnel";
 
@@ -12,53 +12,56 @@ import "../styles/charts.css";
 function D3FunnelChart() {
   const chartRef = useRef(null);
 
-  useEffect(() => {
-    if (chartRef.current) {
-      const chartOptions = {
-        chart: {
-          animate: 333,
-          bottomPinch: 1, // demo-2
-          curve: {
-            enabled: true, // demo-1
-            height: 20,
-          },
-          inverted: false, // demo-3
-          height: 300,
-          width: 500,
+  const chartOptions = useMemo(() => {
+    return {
+      chart: {
+        animate: 333,
+        bottomPinch: 1, // demo-2
+        curve: {
+          enabled: true, // demo-1
+          height: 20,
         },
-        block: {
-          barOverlay: true, // demo-4
-          dynamicHeight: false, // demo-5
-          fill: {
-            type: "gradient", // demo-6  "gradient"
-            scale: ["#607ac8", "#188c7c", "#5f842e", "#937530"], // demo-0 (csx chart-colors)
-          },
-          highlight: true, // demo-7
+        inverted: false, // demo-3
+        height: 300,
+        width: 500,
+      },
+      block: {
+        barOverlay: true, // demo-4
+        dynamicHeight: false, // demo-5
+        fill: {
+          type: "gradient", // demo-6  "gradient"
+          scale: ["#607ac8", "#188c7c", "#5f842e", "#937530"], // demo-0 (csx chart-colors) - get from css export
         },
-        events: {
-          click: {
-            block(event, d) {
-              window.open("https://security.cisco.com/", "_blank");
-            },
+        highlight: true, // demo-7
+      },
+      events: {
+        click: {
+          block(evt, d) {
+            window.open("https://security.cisco.com/", "_blank");
           },
         },
-        label: {
-          enabled: true, // demo-8
-          fill: "#fff", // use #000 for dark theme ?!
-          fontSize: "12px",
-        },
-        tooltip: {
-          enabled: true,
-          format: "{f} are {l}", // takes func or f-str
-        },
-      };
-      const chart = new D3Funnel("#funnel");
-      chart.draw(threat_hunt_data, chartOptions);
-      // chart.draw(sales_process_data, chartOptions);
-    }
+      },
+      label: {
+        enabled: true, // demo-8
+        fill: "#fff", // use #000 for dark theme ?! // jsx tertiary
+        fontSize: "12px",
+      },
+      tooltip: {
+        enabled: true,
+        format: "{f} are {l}", // takes func or f-str
+      },
+    };
   }, []);
 
-  return <div id="funnel" ref={chartRef} className="chart-container"></div>;
+  useEffect(() => {
+    // key dev distinction to check for the .current in a useEffect (whiteboard!)
+    if (chartRef.current) {
+      const chart = new D3Funnel("#funnel"); // check it with the new keyword (class style)
+      chart.draw(threat_hunt_data, chartOptions); // key draw() method
+    }
+  }, [chartOptions]);
+
+  return <div id="funnel" ref={chartRef} className="chart-container"></div>; // container div
 }
 
 export default D3FunnelChart;
